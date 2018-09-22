40245f6aeb7d4835190c3497f45afa383aca9297
fauzisunarya <fauzialdzikirullah@gmail.com>
2018-09-22 17:18:01 +0700

    first commit

---
+++ src/pkgif/pkg3/pkg10117110/latihan13/relational/IF310117110Latihan13Relational.java
@@ -0,0 +1,28 @@
+
+package pkgif.pkg3.pkg10117110.latihan13.relational;
+
+/**
+ *
+ * @author
+ * Nama : Fauzi Sunarya
+ * Kelas : IF-3
+ * NIM : 10117110
+ * Deskripsi Program : Program Jenis ini Berisi Program Untuk Menampilkan
+ * sebuah relasi antar bilangan
+ */
+public class IF310117110Latihan13Relational {
+
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("a >= b = " + (bil2 >= bil1));
+        System.out.println("a <= b = " + (bil2 <= bil1));
+
+
+    }
+    
+}
